7c8de37310679842e189b9599c2d6dc608b34d83
ravikmrg6-eng <ravikmrg6@gmail.com>
2026-04-09 18:18:27 +0000

    first commit

--- cp-predictive-maintenance-proj/deployment/app.py
+++ cp-predictive-maintenance-proj/deployment/app.py
@@ -18,11 +18,11 @@ Please enter the app details below to get a prediction of engine health.
 # User input
 
 Engine_RPM = st.number_input("Indicating the Engine Speed in RPM ", min_value=1, max_value=2000, value=1, step=1)
-Lub_Oil_Pressure = st.number_input("The pressure of the Lubricating oil in the engine in kPa", min_value=1.000000, max_value=20.000000, value=1.000000, step=0.000001)
-Fuel_Pressure = st.number_input("The Fuel Pressure in kPa", min_value=1.000000, max_value=20.000000, value=1.000000, step=0.000001)
-Coolant_Pressure = st.number_input("The Pressure of the Engine Coolant in kPa", min_value=1.000000, max_value=20.000000, value=1.000000, step=0.000001)
-Lub_Oil_Temperature = st.number_input("The Temparture of the Lub Oil", min_value=1.000000, max_value=100.000000, value=1.000000, step=0.000001)
-Coolant_Temperature = st.number_input("The Temparture of the Engine Coolant",  min_value=1.000000, max_value=100.000000, value=1.000000, step=0.000001)
+Lub_Oil_Pressure = st.number_input("The pressure of the Lubricating oil in the engine in kPa", min_value=1.0000000000, max_value=20.0000000000, value=1.0000000000, step=0.0000000001 , format="%.10f" )
+Fuel_Pressure = st.number_input("The Fuel Pressure in kPa", min_value=1.000000, max_value=20.000000, value=1.000000, step=0.000001, format="%.6f" )
+Coolant_Pressure = st.number_input("The Pressure of the Engine Coolant in kPa", min_value=1.000000, max_value=20.000000, value=1.000000, step=0.000001, format="%.6f")
+Lub_Oil_Temperature = st.number_input("The Temparture of the Lub Oil", min_value=1.000000, max_value=100.000000, value=1.000000, step=0.000001, format="%.6f")
+Coolant_Temperature = st.number_input("The Temparture of the Engine Coolant",  min_value=1.000000, max_value=100.000000, value=1.000000, step=0.000001, format="%.6f")
 
 # Assemble input into DataFrame
 input_data = pd.DataFrame([{
